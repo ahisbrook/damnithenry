251691090028150fe61350e01f7b2d3f7a015d34
ahisbrook <ahisbrook@gmail.com>
2015-07-02 12:47:59 -0500

    damnit henry

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ class DamnitHenry < Sinatra::Base
   set :show_exceptions, true
 
   get '/' do
-    "Damnit Henry! (╯°□°）╯︵ ┻━┻"
+    erb :index
   end
+
+  post '/' do
+    puts params
+    erb :apology, :locals => {
+      :henry_did => params["henry_did"]
+    }
+  end
+
 end
