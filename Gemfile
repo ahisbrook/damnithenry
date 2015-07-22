420bd74d789482b3e6c4c032a437465d7a3e378c
ahisbrook <ahisbrook@gmail.com>
2015-07-22 11:31:05 -0500

    Added twitter integration

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 ruby '2.0.0'
 
 gem 'sinatra'
+gem 'twitter'
 
 group :test do
   gem 'pry'
--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'twitter'
 
 lib_path = File.expand_path('../lib', __FILE__)
 require "#{lib_path}/apology_generator"
@@ -16,9 +17,21 @@ class DamnitHenry < Sinatra::Base
 
   post '/' do
     apology = ApologyGenerator.new.generate(params["henryDidWhat"])
+    twitter.update("#{apology} -- via damnithenry.com")
     erb :index, :locals => {
       :apology_visibility => 'visible',
       :apology => apology
     }
+  end
+
+  private
+
+  def twitter
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
   end
 end
